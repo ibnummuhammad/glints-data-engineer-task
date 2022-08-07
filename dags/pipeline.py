f30d6033e49f23c90f686bf90dd91770ade46c84
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2022-08-07 13:59:59 +0700

    Fix extract data task

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -9,7 +9,7 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
-def _extract_table_source():
+def _extract_data():
     sql_stmt = "SELECT * FROM sales"
     pg_hook = PostgresHook(postgres_conn_id="postgres_src", schema="airflow")
     pg_conn = pg_hook.get_conn()
@@ -18,15 +18,9 @@ def _extract_table_source():
     data_raw = cursor.fetchall()
     data = []
     for row in data_raw:
-        data.append(
-            {
-                "id": row[0],
-                "quantity": row[1],
-                "price": row[2],
-                "date": row[3].strftime("%Y-%m-%d"),
-            }
-        )
-    data = json.dumps(data)
+        data.append(tuple((row[0], row[1], row[2], row[3].strftime("%Y-%m-%d"))))
+    data = str(data)
+    data = data[1:-1]
     return data
 
 
@@ -63,10 +57,16 @@ with DAG(
             FROM '/opt/data/sales_september_2019.csv' DELIMITER ',' CSV HEADER;
         """,
     )
-    extract_table_source = PythonOperator(
-        task_id="extract_table_source",
-        python_callable=_extract_table_source,
+    extract_data = PythonOperator(
+        task_id="extract_data",
+        python_callable=_extract_data,
         do_xcom_push=True,
+    )
+    load_data = PostgresOperator(
+        task_id="load_data",
+        postgres_conn_id="postgres_dst",
+        sql="""INSERT INTO sales VALUES
+                ('{{ ti.xcom_pull(task_ids='extract_data', key='return_value') }}');""",
     )
     end_task = DummyOperator(task_id="end")
 
@@ -74,6 +74,7 @@ with DAG(
     start_task
     >> create_table_source
     >> import_table_source
-    >> extract_table_source
+    >> extract_data
+    >> load_data
     >> end_task
 )
