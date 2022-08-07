d95c2beb31e2da5ca7b5df249194fbe1a5258877
Ibnu Muhammad <ibnu0muhammad@gmail.com>
2022-08-07 12:11:31 +0700

    Add create table task

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -3,24 +3,33 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 
 with DAG(
-    "data_pipeline",
+    "pipeline",
     default_args={
         "depends_on_past": False,
         "retries": 1,
-        "retry_delay": timedelta(minutes=5),
     },
     description="A simple tutorial DAG",
     start_date=datetime(2022, 8, 5),
     catchup=False,
 ) as dag:
     start_task = DummyOperator(task_id="start")
-    print_hello_world = BashOperator(
-        task_id="print_hello_world", bash_command='echo "HelloWorld!"'
+    create_table = PostgresOperator(
+        task_id="create_table",
+        postgres_conn_id="postgres_src",
+        sql="""
+            CREATE TABLE IF NOT EXISTS sales(
+                id SERIAL,
+                quantity INT,
+                price INT,
+                date DATE,
+                PRIMARY KEY (id)
+            );
+        """,
     )
     end_task = DummyOperator(task_id="end")
 
-start_task >> print_hello_world
-print_hello_world >> end_task
+start_task >> create_table >> end_task
